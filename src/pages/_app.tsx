1dbd2e003dd87666634cc8f383bd93145d6aec26
Jack <k4667333@gmail.com>
2023-02-16 03:46:34 +0800

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import App from 'next/app'
+import {
+  ParallaxProvider,
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+} from "react-scroll-parallax";
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props
+    return (<ParallaxProvider>
+      <Component {...pageProps} />
+    </ParallaxProvider>)
+  }
 }
+export default MyApp
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'yellow': '#FF8002',
+      'gold':'#8D4200'
+    },
+  },
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+  ],
+}
